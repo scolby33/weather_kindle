0042ba020254d2f3a1d28060d7a81ab40efa7999
Scott Colby <scolby33@gmail.com>
2017-11-15 20:15:04 -0500

    Add config file for location and use it in update_weather.sh

--- weather/bin/update_weather.sh
+++ weather/bin/update_weather.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ZIP=10001
-
 cd /mnt/us/weather
+
+source etc/weather_config
 
 # clean up old images
 rm img/weather_out.svg.old
@@ -15,7 +15,25 @@ mv img/weather_out.png img/weather_out.png.old
 mv img/weather.png img/weather.png.old
 
 # downloads the weather and outputs to img/weather_out.svg
-/mnt/us/weather/bin/download_weather.py --rotated --template img/weather_template.svg ${ZIP} > img/weather_out.svg
+OPTIONS="--template img/weather_template.svg"
+if [ "${ROTATED:-0}" -ge 1 ]
+then
+    OPTIONS="$OPTIONS --rotated"
+fi
+
+if [ -n "${ZIP+x}" ]
+then
+    bin/download_weather.py ${OPTIONS} -- "${ZIP}" > img/weather_out.svg
+elif [ -n "${LAT+x}" ] && [ -n "${LON+x}" ]
+then
+    bin/download_weather.py ${OPTIONS} -- "${LAT}" "${LON}" > img/weather_out.svg
+elif [ -n "${LOCATION+x}" ] && [ -n "${LOCATION+x}" ]
+then
+    bin/download_weather.py ${OPTIONS} --key "${KEY}" -- "${LOCATION}" > img/weather_out.svg
+else
+    >&2 echo "No valid locations defined"
+    >&2 echo "Check etc/weather_config"
+fi
 
 # convert the svg to a png with white background (no transparency allowed!)
 /mnt/us/weather/bin/rsvg-convert --background-color=white -o img/weather_out.png img/weather_out.svg
