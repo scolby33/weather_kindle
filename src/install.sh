133c09a34e95cf54f1a95549046809cc6303b1f8
Scott Colby <scott@scolby.com>
2019-11-10 13:40:41 -0500

    Installer rewrite

--- src/install.sh
+++ src/install.sh
@@ -1,39 +1,61 @@
 #!/bin/sh
 
-[ -f ./libotautils ] && source ./libotautils
+[ -f ./libotautils ] && . ./libotautils
 
 HACKNAME="weather"
+HACKVER="1.0.0"
 
+# Directories
+WEATHER_BASEDIR="/mnt/us/$HACKNAME"
+EXTENSIONS_BASEDIR="/mnt/us/extensions/$HACKNAME"
+
+# Files
+WEATHER_CONFIG="$WEATHER_BASEDIR/etc/weather_config.sh"
+WEATHER_CONFIG_BU="$WEATHER_BASEDIR/../weather_config.sh.orig"
+
+# remove existing install, preserving config
 otautils_update_progressbar
-# Remove existing install
-logmsg "I" "install" "" "removing existing weather install..."
-if [ -d "/mnt/us/weather" ]; then
-    rm -rf "/mnt/us/weather"
+logmsg "I" "install" "" "Removing existing weather install"
+if [ -f "$WEATHER_CONFIG" ]; then
+    logmsg "I" "install" "" "Found existing config; preserving it"
+    cp "$WEATHER_CONFIG" "$WEATHER_CONFIG_BU"
 fi
+rm -rf "$WEATHER_BASEDIR"
 
+# install new version
 otautils_update_progressbar
-# Install Weather in the userstore
-logmsg "I" "install" "" "unpacking weather..."
+logmsg "I" "install" "" "Unpacking weather to user store"
 # Make sure xzdec is executable first
-chmod +x ./xzdec
-./xzdec "${HACKNAME}.tar.xz" | tar -xvf - -C /mnt/us/
+chmod +x xzdec; ./xzdec "$HACKNAME.tar.xz" | tar -xvf - -C /mnt/us
 _RET=$?
-if [ ${_RET} -ne 0 ]; then
-    logmsg "C" "install" "code=${_RET}" "failed to update userstore with custom directory"
-    echo "${_RET}" >> $WLOG
-    return 1
+if [ "$_RET" -ne 0 ]; then
+    logmsg "C" "install" "code=$_RET" "Failed to unpack weather."
+    exit 1
+fi
+# backup default config
+cp -f "$WEATHER_CONFIG" "$WEATHER_CONFIG.default"
+
+# replace config if necessary
+otautils_update_progressbar
+if [ -f "$WEATHER_CONFIG_BU" ]; then
+    logmsg "I" "install" "" "Restoring original config"
+    mv "$WEATHER_CONFIG_BU" "$WEATHER_CONFIG"
 fi
 
+# make sure everything needed is executable
 otautils_update_progressbar
-# Backup default config
-cp -f /mnt/us/weather/etc/weather_config /mnt/us/weather/etc/weather_config.default
+logmsg "I" "install" "" "Marking executables as executable"
+chmod -R +x "$WEATHER_BASEDIR/bin" "$EXTENSIONS_BASEDIR/bin"
 
+# clean up
 otautils_update_progressbar
-logmsg "I" "install" "" "cleaning up"
-rm -f "${HACKNAME}.tar.xz" "xzdec"
+logmsg "I" "install" "" "Cleaning up"
+rm -f "$HACKNAME.tar.xz" "xzdec"
 
+# done
 otautils_update_progressbar
-logmsg "I" "install" "" "done"
+logmsg "I" "install" "" "Done!"
 
 otautils_update_progressbar
 return 0
+
