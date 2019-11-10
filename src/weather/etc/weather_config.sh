5390838b168b2c82256da0a64bbad4c00e15460e
Scott Colby <scott@scolby.com>
2019-11-10 00:54:19 -0500

    Update weather_config for new variables and better comments

--- src/weather/etc/weather_config.sh
+++ src/weather/etc/weather_config.sh
@@ -1,35 +1,58 @@
+##################
+# Global Options #
+##################
+
 # Uncomment if you want the output roated 180 degrees
 # i.e. if you mounted your Kindle upside down
+# This variable is checked for being set or unset;
+# the value does not matter. For example,
+# `ROTATED=0` would also count as set.
 #ROTATED=1
 
 ##################################################
 # Uncomment and set ONE of the following options #
 ##################################################
 
-##############################
+#----------------------------#
 # Weather.gov using ZIP code #
-##############################
+#----------------------------#
 # Only works in the US
 
 ZIP=10001
 
-############################################
+#------------------------------------------#
 # Weather.gov using Latitude and Longitude #
-############################################
+#------------------------------------------#
 # Only works in the US
 
 #LAT=40.7515634
 #LON=-74.0047868
 
-#######################
-# Weather Underground #
-#######################
-# Use a location specifier as shown under "query" at
-# https://www.wunderground.com/weather/api/d/docs?d=data/index
-# Get a Weather Underground key--a free developer-level "Stratus"
-# plan should be sufficient--from here
-# https://www.wunderground.com/weather/api/
-# This is the only option for weather outside the US
+#-------------#
+# AccuWeather #
+#-------------#
+# Works worldwide!
 
-#LOCATION=NY/New_York
+# Get an API key from https://www.developer.accuweather.com.
+# A free "Limited Trial" plan seems to be sufficient.
+# Fill that value in here
 #KEY=12345
+
+# Find the "location key" for your location.
+# This can be found by viewing the source of the AccuWeather page
+# for your location and finding the canonical link:
+# <link rel="canonical" href="https://www.accuweather.com/en/us/new-york/10007/weather-forecast/349727" />
+# The final component of the URL, in this case 349727, appears to be the key.
+# The "correct" method of finding the location key is to use the Locations API.
+# https://developer.accuweather.com/accuweather-locations-api/apis
+# You'll only need to do this once, so using the online form from the
+# documentation should be sufficient.
+#LOCATION=349727
+
+# Uncomment if you want metric units
+# (Celsius instead of Farhenheit)
+# Only works with AccuWeather
+# This variable is checked for being set or unset;
+# the value does not matter. For example,
+# `ROTATED=0` would also count as set.
+#METRIC=1
